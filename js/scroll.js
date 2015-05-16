08cc5c1bf5eca58ce21de71290bf1ae319f2dd89
Maritza <maritzasintafur@gmail.com>
2015-05-15 23:38:31 -0500

    initial commit

---
+++ js/scroll.js
@@ -0,0 +1,31 @@
+/*change color menú*/
+$(document).scroll(function() {    
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 350) {
+        $("#scroll").addClass("active");
+    } else {
+        $("#scroll").removeClass("active");
+    }
+
+});
+
+/*Menú Toggle*/
+$(".switch").click(function() {
+  $(this).toggleClass("on");
+});
+
+/*Scroll top*/
+$(document).on('click', 'a.smooth', function(e) {
+	e.preventDefault();
+    var $link = $(this);
+    var anchor  = $link.attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top
+    }, 1000);
+});
+
+$('.imagen').hover(function(){
+    $(".overlay").toggleClass('hidden');
+    $(".overlay").toggleClass("active");
+});
