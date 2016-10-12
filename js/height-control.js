a162b21fc91f1833391f30a5b0b4395fbc30ea8a
maritzavst <maritzasintitafur@gmail.com>
2016-10-12 06:06:27 -0500

    cambios de responsive movil

--- js/height-control.js
+++ js/height-control.js
@@ -1,6 +1,5 @@
 $(window).on("load", function(){
 	var maxHeight=0;
-	"use strict"
 	var image = $(".proyecto").find("img");
 	$(".proyecto").each(function(){
 		var heightImage = $(this).find("img").height();
